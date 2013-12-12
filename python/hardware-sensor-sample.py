e9360fb3cbd391384185e821bf721ff244da2d9e
Mark Chmarny <mark@chmarny.com>
2013-12-12 12:14:54 -0800

    Update hardware-sensor-sample.py

--- python/hardware-sensor-sample.py
+++ python/hardware-sensor-sample.py
@@ -1,3 +1,34 @@
+# Copyright © 2013 enableiot.com
+# All rights reserved under the copyright laws 
+# of the United States and applicable international 
+# laws, treaties, and conventions.
+# You may freely redistribute and use this sample 
+# code, with or without modification, provided you 
+# include the original copyright notice and use 
+# restrictions.
+# 
+# Disclaimer: THE SAMPLE CODE IS PROVIDED "AS IS," 
+#             AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+#             INCLUDING THE IMPLIED WARRANTIES OF 
+#             MERCHANTABILITY AND FITNESS FOR A 
+#             PARTICULAR PURPOSE, ARE DISCLAIMED. 
+#             IN NO EVENT SHALL ENABLEIOT OR 
+#             CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+#             INDIRECT, INCIDENTAL, SPECIAL, 
+#             EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+#             (INCLUDING, BUT NOT LIMITED TO, 
+#             PROCUREMENT OF SUBSTITUTE GOODS OR 
+#             SERVICES; LOSS OF USE, DATA, OR 
+#             PROFITS; OR BUSINESS INTERRUPTION) 
+#             SUSTAINED BY YOU OR A THIRD PARTY, 
+#             HOWEVER CAUSED AND ON ANY THEORY OF 
+#             LIABILITY, WHETHER IN CONTRACT, 
+#             STRICT LIABILITY, OR TORT, ARISING 
+#             IN ANY WAY OUT OF THE USE OF THIS 
+#             SAMPLE CODE, EVEN IF ADVISED OF THE 
+#             POSSIBILITY OF SUCH DAMAGE.
+
+
 import os
 import socket
 import time
