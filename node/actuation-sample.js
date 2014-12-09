7ae49fb2ee593bd4528062d6ec300cbf0a49df6b
Brian Baltz <brian.a.baltz@intel.com>
2014-12-09 22:18:42 +0000

    Adding Python and Node.js actuation examples

---
+++ node/actuation-sample.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+/*
+ Copyright (c) 2012, Intel Corporation
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+ this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ * Neither the name of Intel Corporation nor the names of its contributors
+ may be used to endorse or promote products derived from this software
+ without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+"use strict";
+var endpoint = "127.0.0.1"
+var port     = 41235
+
+var m = require('mraa'); //require mraa
+
+var myLed = new m.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
+myLed.dir(m.DIR_OUT); //set the gpio direction to output
+
+var sensorName = "LED";
+
+// listen for UDP message from local agent
+var dgram = require("dgram");
+var server = dgram.createSocket("udp4");
+
+server.on("error", function (err) {
+  console.log("server error:\n" + err.stack);
+  server.close();
+});
+
+server.on("message", function (msg, rinfo) {
+  console.log("server got: " + msg + " from " +
+    rinfo.address + ":" + rinfo.port);
+  // Ignore essages unless they are local
+  if(rinfo.address != "127.0.0.1") {
+    console.log("Ignoring remote UDP message");
+    return;
+  }
+  var js = JSON.parse(msg);
+  var component = js['component'];
+  var command = js['command'];
+  var argvArray = js['argv'];
+  console.log("component: " + component);
+  console.log("command: " + command);
+  for(var i = 0; i < argvArray.length; i++) {
+    var name = argvArray[i]['name']
+    var value = argvArray[i]['value']
+    console.log("name: " + name);
+    console.log("value: " + value);
+    if (name == sensorName) {
+        myLed.write(parseInt(value));
+    }
+  }
+
+});
+
+server.on("listening", function () {
+  var address = server.address();
+  console.log("server listening " +
+      address.address + ":" + address.port);
+});
+
+server.bind(port);
+
