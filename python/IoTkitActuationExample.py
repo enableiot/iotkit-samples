25000475c67fa98a0f4b6516c60cec9996e860d5
Brian Baltz <brian.a.baltz@intel.com>
2014-12-09 22:18:42 +0000

    Adding Python and Node.js actuation examples
    Update copyright dates

---
+++ python/IoTkitActuationExample.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# Copyright (c) 2014, Intel Corporation
+
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+
+#    * Redistributions of source code must retain the above copyright notice,
+#      this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation
+#      and/or other materials provided with the distribution.
+#    * Neither the name of Intel Corporation nor the names of its contributors
+#      may be used to endorse or promote products derived from this software
+#      without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import socket
+import mraa
+import json
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 41235
+
+myLed = mraa.Gpio(13)  #LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
+myLed.dir(mraa.DIR_OUT)    #set the gpio direction to output
+
+# Sensor and parameter names to listen for
+componentName = "led1"
+sensorName = "LED"
+
+# Bind to UDP port 41235
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+print "Listening on port", UDP_PORT
+
+while True:
+    data,addr = sock.recvfrom(4096)
+    print "Received ", data, "from", addr[0]
+    if addr[0] != "127.0.0.1":
+        print "Rejecting external UDP message from", addr[0]
+        continue
+    js = json.loads(data)
+    component = js["component"]
+    command = js["command"]
+    argvArray = js["argv"]
+    if component == componentName:
+        for argv in argvArray:
+            name = argv['name']
+            value = argv['value']
+            print "name: " + name
+            print "value: " + value
+            if (name == sensorName):
+                myLed.write(int(value))
