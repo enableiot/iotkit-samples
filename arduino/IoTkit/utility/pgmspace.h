bbc4fd00d81d8c2c25cb5cc0f2215d2c1947e07f
Marek Tomczewski <marek.tomczewski@intel.com>
2015-05-20 10:21:20 +0200

    Updated pgmspace.h with new version with known license. Changed license in ThirdPartyLicenses. Removed LGPL as no component uses this license.

--- arduino/IoTkit/utility/pgmspace.h
+++ arduino/IoTkit/utility/pgmspace.h
@@ -1,3 +1,29 @@
+/*
+  pgmspace.h - Definitions for compatibility with AVR pgmspace macros
+
+  Copyright (c) 2015 Arduino LLC
+
+  Based on work of Paul Stoffregen on Teensy 3 (http://pjrc.com)
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE
+*/
+
 #ifndef __PGMSPACE_H_
 #define __PGMSPACE_H_ 1
 
@@ -24,7 +50,7 @@ typedef uint32_t prog_uint32_t;
 #define strcat_P(dest, src) strcat((dest), (src))
 #define strcmp_P(a, b) strcmp((a), (b))
 #define strstr_P(a, b) strstr((a), (b))
-#define strlen_P(s) strlen((const char *)(s))
+#define strlen_P(a) strlen((a))
 #define sprintf_P(s, f, ...) sprintf((s), (f), __VA_ARGS__)
 
 #define pgm_read_byte(addr) (*(const unsigned char *)(addr))
@@ -42,4 +68,3 @@ typedef uint32_t prog_uint32_t;
 #define pgm_read_float_far(addr) pgm_read_float(addr)
 
 #endif
-
